d1702112e1c97d68ab4a16a31647cb2dd874ae4d
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2025-03-11 13:10:41 +0100

    msvc-view-error: Initial version

---
+++ msvc-view-error/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <ranges>
+#include <string>
+#include <vector>
+
+
+struct Token
+{
+    std::string value;
+    bool used = false;
+};
+
+int main()
+{
+    auto tokens = std::vector{Token{"p1"}, Token{"p2"}, Token{"++"}, Token{"p3"}};
+
+    auto view = tokens
+        | std::views::drop_while([](auto const & token) { return token.used; })
+        | std::views::take_while([](auto const & token) { return !token.used; })
+        | std::views::filter([](auto const & token)
+          {
+              if (token.value != "++")
+              {
+                  return true;
+              }
+              if (token.value == "++")
+              {
+                  return false;
+              }
+              return !token.value.starts_with("+");
+          });
+
+    auto transform = view
+        | std::views::transform([](auto & token)
+          {
+              return token.value;
+          })
+        | std::views::common;
+
+    // auto strs = std::vector(transform.begin(), transform.end());
+
+    // for (auto const & str : strs)
+    // {
+    //     std::cout << str << ", ";
+    // }
+
+    return 0;
+}
