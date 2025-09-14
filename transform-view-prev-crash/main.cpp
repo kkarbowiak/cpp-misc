42ace26fa107c1e10d4eab34f8f96d54be41a07b
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2025-09-14 23:21:22 +0200

    transform-view-prev-crash: Initial version

---
+++ transform-view-prev-crash/main.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <memory>
+#include <ranges>
+#include <string>
+#include <vector>
+
+
+class Fmt
+{
+    public:
+        virtual auto get_name() const -> std::string const & = 0;
+    
+    protected:
+        virtual ~Fmt() = default;
+};
+
+struct Options
+{
+    std::string name;
+};
+
+class OptionsHolder
+{
+    protected:
+        explicit OptionsHolder(Options options)
+          : m_options(std::move(options))
+        {
+        }
+
+        auto get_name() const -> std::string const &
+        {
+            return m_options.name;
+        }
+
+    private:
+        Options m_options;
+};
+
+class Base : public Fmt, public OptionsHolder
+{
+    public:
+        explicit Base(Options options)
+          : OptionsHolder(std::move(options))
+        {
+        }
+        virtual ~Base() = default;
+
+        auto get_name() const -> std::string const & override
+        {
+            return OptionsHolder::get_name();
+        }
+};
+
+class Der1 final : public Base
+{
+    public:
+        explicit Der1(Options options)
+          : Base(std::move(options))
+        {
+        }
+};
+
+class Der2 final : public Base
+{
+    public:
+        explicit Der2(Options options)
+          : Base(std::move(options))
+        {
+        }
+};
+
+auto print_names(std::ranges::view auto ders) -> void
+{
+    for (auto it = ders.begin(); it != ders.end(); ++it)
+    {
+        auto const & d = *it;
+        std::cout << d->get_name() << '\n';
+        if (it != ders.begin())
+        {
+            (void) std::prev(it);
+        }
+    }
+}
+
+int main()
+{
+    std::vector<std::unique_ptr<Base>> ders;
+    ders.push_back(std::make_unique<Der1>(Options{"Der1"}));
+    ders.push_back(std::make_unique<Der2>(Options{"Der2"}));
+
+    print_names(ders | std::views::transform([](auto const & ca) { return static_cast<Fmt const *>(ca.get()); }));
+}
