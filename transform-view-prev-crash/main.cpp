1c22620cb9dde2d90bc469e6b30e10dc5390712b
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2025-09-14 23:38:59 +0200

    transform-view-prev-crash: Remove name altogether

--- transform-view-prev-crash/main.cpp
+++ transform-view-prev-crash/main.cpp
@@ -6,9 +6,6 @@
 
 class Fmt
 {
-    public:
-        virtual auto get_name() const -> std::string const & = 0;
-    
     protected:
         virtual ~Fmt() = default;
 };
@@ -16,26 +13,15 @@ class Fmt
 class Base : public Fmt
 {
     public:
-        explicit Base(std::string name)
-          : m_name(std::move(name))
-        {
-        }
+        Base() = default;
         virtual ~Base() = default;
-
-        auto get_name() const -> std::string const & override
-        {
-            return m_name;
-        }
-
-    private:
-        std::string m_name;
 };
 
 class Der final : public Base
 {
     public:
-        explicit Der(std::string name)
-          : Base(std::move(name))
+        Der()
+          : Base()
         {
         }
 };
@@ -54,8 +40,8 @@ auto print_names(std::ranges::view auto ders) -> void
 int main()
 {
     std::vector<std::unique_ptr<Base>> ders;
-    ders.push_back(std::make_unique<Der>("1"));
-    ders.push_back(std::make_unique<Der>("2"));
+    ders.push_back(std::make_unique<Der>());
+    ders.push_back(std::make_unique<Der>());
 
     print_names(ders | std::views::transform([](auto const & ca) { return static_cast<Fmt const *>(ca.get()); }));
 }
