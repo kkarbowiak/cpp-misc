f1615c8fdf8032c6b1fb4160d2d68263607bbe0a
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2025-09-14 23:24:29 +0200

    transform-view-prev-crash: Simplify loop

--- transform-view-prev-crash/main.cpp
+++ transform-view-prev-crash/main.cpp
@@ -73,8 +73,6 @@ auto print_names(std::ranges::view auto ders) -> void
 {
     for (auto it = ders.begin(); it != ders.end(); ++it)
     {
-        auto const & d = *it;
-        std::cout << d->get_name() << '\n';
         if (it != ders.begin())
         {
             (void) std::prev(it);
